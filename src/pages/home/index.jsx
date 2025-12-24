50bf334e5d83e8f31c4fc39c1137771186ab735f
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-24 19:25:54 +0500

    .

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,9 +4,9 @@ import ProductCard from '../../components/productCard'
 const Home = () => {
   const productsData = [
     { price: 49, name: "Aukat Bahar Nihari", image:"https://javednihari.com/_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fupload%2F1697440665-_0008_7a.jpg%3Fq%3D10&w=384&q=75"},
-    { price: 49, name: "Aukat Bahar Nihari", image:"https://javednihari.com/_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fupload%2F1697440665-_0008_7a.jpg%3Fq%3D10&w=384&q=75"},
-    { price: 49, name: "Aukat Bahar Nihari", image:"https://javednihari.com/_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fupload%2F1697440665-_0008_7a.jpg%3Fq%3D10&w=384&q=75"},
-    { price: 49, name: "Aukat Bahar Nihari", image:"https://javednihari.com/_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fupload%2F1697440665-_0008_7a.jpg%3Fq%3D10&w=384&q=75"},
+    { price: 49, name: "Aukat Bahar Nihari", image:"https://javednihari.com/_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fupload%2F1697443215-Nali%20Nihari%20-min.png%3Fq%3D10&w=384&q=75"},
+    { price: 49, name: "Aukat Bahar Nihari", image:"https://javednihari.com/_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fupload%2F1697443261-magaz%20Nihari-min.png%3Fq%3D10&w=384&q=75"},
+    { price: 49, name: "Aukat Bahar Nihari", image:"https://javednihari.com/_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fupload%2F1697443304-Special%20Nihari-min.png%3Fq%3D10&w=384&q=75"},
   ]
   return (
     <>
