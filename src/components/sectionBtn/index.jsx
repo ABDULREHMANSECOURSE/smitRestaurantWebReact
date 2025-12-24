2a4421914c5994d14e8754c2200b0175c264b066
Abdul Rehman SE Course <abdulrehmansecourse@gmail.com>
2025-12-25 00:59:36 +0500

    .

---
+++ src/components/sectionBtn/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SectionBtn = ({ text, link }) => {
+    return (
+        <a href={link}>{text}</a>
+    )
+}
+
+export default SectionBtn
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProductCard from '../../components/productCard'
+import SectionBtn from '../../components/sectionBtn'
 
 const Home = () => {
   const PProductsData = [
@@ -8,8 +9,22 @@ const Home = () => {
     { price: 1050, name: "Maghaz Nihari", image: "https://javednihari.com/_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fupload%2F1697443261-magaz%20Nihari-min.png%3Fq%3D10&w=384&q=75" },
     { price: 1450, name: "Special Nihari", image: "https://javednihari.com/_next/image?url=https%3A%2F%2Fassets.indolj.io%2Fupload%2F1697443304-Special%20Nihari-min.png%3Fq%3D10&w=384&q=75" },
   ]
+  const sectionBtnText = [
+    { text: "Nihari", link: "nihari" },
+    { text: "Haleem", link: "Haleem" },
+    { text: "Biryani / Pulao", link: "biryaniPulao" },
+    { text: "Tandoor", link: "tandoor" },
+    { text: "Sweet Dishes", link: "sweetDishes" },
+    { text: "Soft Drink", link: "softDrink" },
+    { text: "Tin Pack", link: "tinPack" }
+  ]
   return (
     <>
+      <span className='categoryNav'>
+        {sectionBtnText.map((iten, index) => {
+          return <SectionBtn text={iten.text} />
+        })}
+      </span>
       <span className='pProducts'>
         {PProductsData.map((item, index) => [
           <ProductCard name={item.name} price={item.price} image={item.image} />
